6e3162e1fce59b34e27f977b930bcedeb596688d
AnnaMedvedeva <anna_alekseevna@MacBook-Air-Anna.local>
2024-06-07 20:53:09 +0300

    добавили проверки в тест сделали код получения активационного токена

--- test_post_v1_account.py
+++ test_post_v1_account.py
@@ -1,11 +1,14 @@
+import pprint
+
 import requests
+from json import loads
 
 
 def test_post_v1_account():
     # Регистрация пользователя
-    login='medvedeva_test'
-    password='123456789'
-    email=f'{login}t@mail.ru'
+    login = 'medvedeva_test5'
+    password = '123456789'
+    email = f'{login}t@mail.ru'
     json_data = {
         'login': login,
         'email': email,
@@ -15,24 +18,35 @@ def test_post_v1_account():
     response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
 
     # Получить письма из почтового сервера
 
     params = {
-        'limit': '50',
+        'limit': '50'
     }
 
     response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
     print(response.status_code)
     print(response.text)
-    # Получить активационный токен
-    ...
+    assert response.status_code == 200, "Письма не были получены"
 
+    # Получить активационный токен
+    token = None
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+
+    assert token is not None, f"Токен для пользователя {login}, не был получен"
     # Активация пользователя
 
-    response = requests.put('http://5.63.153.31:5051/v1/account/7d87f459-e627-4917-873f-729d0f855515')
+    response = requests.put(f'http://5.63.153.31:5051/v1/account/{token}')
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, "Пользователь не был активирован"
 
     # Авторизоваться
 
@@ -45,3 +59,4 @@ def test_post_v1_account():
     response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, "Пользователь не смог авторизоваться"
