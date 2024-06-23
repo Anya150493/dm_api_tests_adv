71f35ed2322157d7d4833bb6d001ff7bf156b09e
AnnaMedvedeva <anna_alekseevna@MacBook-Air-Anna.local>
2024-06-23 18:56:58 +0300

    создали фикстуры авторизации и добавили новые тесты

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -44,6 +44,23 @@ class AccountHelper:
         self.dm_account_api = dm_account_api
         self.mailhog = mailhog
 
+    def auth_client(
+            self,
+            login: str,
+            password: str
+    ):
+        response = self.dm_account_api.login_api.post_v1_account_login(
+            json_data={
+                'login': login,
+                'password': password
+            }
+        )
+        token = {
+            "x-dm-auth-token": response.headers["x-dm-auth-token"]
+        }
+        self.dm_account_api.account_api.set_headers(token)
+        self.dm_account_api.login_api.set_headers(token)
+
     def register_new_user(
             self,
             login: str,
@@ -116,3 +133,74 @@ class AccountHelper:
         response = self.dm_account_api.account_api.put_v1_account_token(token=token)
         assert response.status_code == 200, "Пользователь не был активирован"
         return response
+
+    def logout_user(
+            self,
+            **kwargs
+            ):
+        response = self.dm_account_api.login_api.delete_v1_account_login(**kwargs)
+        assert response.status_code == 204, f"Текущий пользователь не был разлогинен"
+
+    def logout_user_all(
+            self,
+            **kwargs
+            ):
+        response = self.dm_account_api.login_api.delete_v1_account_login_all(**kwargs)
+        assert response.status_code == 204, f"Текущий пользователь не был разлогинен со всех устройств"
+
+    def change_password(
+            self,
+            login: str,
+            email: str,
+            old_password: str,
+            new_password: str
+            ):
+        token = self.user_login(login=login, password=old_password)
+        self.dm_account_api.account_api.post_v1_account_password(
+            json_data={
+                "login": login,
+                "email": email
+            },
+            headers={
+                "x-dm-auth-token": token.headers["x-dm-auth-token"]
+            },
+        )
+        token = self.get_token(login=login, token_type="reset")
+        self.dm_account_api.account_api.put_v1_account_password(
+            json_data={
+                "login": login,
+                "oldPassword": old_password,
+                "newPassword": new_password,
+                "token": token
+            }
+        )
+
+    @retry(
+        stop_max_attempt_number=5, retry_on_result=retry_if_result_none, wait_fixed=1000
+    )
+    def get_token(
+            self,
+            login,
+            token_type="activation"
+    ):
+        """
+        Получение токена активации или сброса пароля
+        Args:
+            login: логин пользователя
+            token_type: тип токена (activation или reset)
+        Returns:
+            токен активации или сброса пароля
+        """
+        token = None
+        response = self.mailhog.mailhog_api.get_api_v2_messages()
+        for item in response.json()["items"]:
+            user_data = loads(item["Content"]["Body"])
+            user_login = user_data["Login"]
+            activation_token = user_data.get("ConfirmationLinkUrl")
+            reset_token = user_data.get("ConfirmationLinkUri")
+            if user_login == login and activation_token and token_type == "activation":
+                token = activation_token.split("/")[-1]
+            elif user_login == login and reset_token and token_type == "reset":
+                token = reset_token.split("/")[-1]
+
+        return token
