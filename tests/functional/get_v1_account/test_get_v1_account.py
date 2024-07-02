0063149b5b82e830c4f5df96b725e805f864cc3d
AnnaMedvedeva <anna_alekseevna@MacBook-Air-Anna.local>
2024-07-02 16:42:33 +0300

    добавила soft_assertions

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,42 +1,20 @@
 from datetime import datetime
 
-from hamcrest import (
-    assert_that,
-    has_property,
-    starts_with,
-    all_of,
-    instance_of,
-    has_properties,
-    equal_to,
-)
-
 from checkers.http_checkers import check_status_code_http
+from assertpy import assert_that, soft_assertions
+
+from dm_api_account.models.user_details_envelope import UserRole
 
 
 def test_get_v1_account_auth(
         auth_account_helper
 ):
-    response = auth_account_helper.dm_account_api.account_api.get_v1_account(validate_response=True)
-    assert_that(
-        response, all_of(
-            has_property('resource', has_property('login', starts_with("medvedeva"))),
-            has_property('resource', has_property('online', instance_of(datetime))),
-            has_property(
-                'resource', has_properties(
-                    {
-                        'rating': has_properties(
-                            {
-                                "enabled": equal_to(True),
-                                "quality": equal_to(0),
-                                "quantity": equal_to(0)
-                            }
-                        )
-                    }
-                )
-            )
-        )
-    )
-    print(response)
+    response = auth_account_helper.dm_account_api.account_api.get_v1_account()
+    with soft_assertions():
+        assert_that(response.resource.login).is_equal_to("medvedeva_26_06_2024_15_48_59")
+        assert_that(response.resource.online).is_instance_of(datetime)
+        assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
+
 
 
 def test_get_v1_account_no_auth(
